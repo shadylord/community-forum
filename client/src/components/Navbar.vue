a3aceff97b0eed8e4109410d2e704c96cd157a4a
Rizky Ramadhan <shady.ramadhan28@gmail.com>
2019-07-04 20:40:55 +0700

    First commit

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+      <a class="navbar-brand" href="/">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor01"
+        aria-controls="navbarColor01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/">Menu 2</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/">Menu 3</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Welcome to Community!</h1>
+      <p
+        class="lead"
+      >This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4" />
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-primary" href="#" role="button">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style scoped>
+</style>
+
